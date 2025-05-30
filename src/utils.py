816709281bcd4b65acba5d25359ef416a421f44a
Srujan3421 <ramagirisrujan123@gmail.com>
2025-05-30 12:43:19 +0530

    Interface for predicting the model

--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,11 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
+import pickle
 
 from sklearn.metrics import r2_score 
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -27,9 +29,12 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         for i in range(len(list(models))):
             model = list(models.values())[i]
             model.fit(X_train,y_train)
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
 
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
            
-
             #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
@@ -47,3 +52,9 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)    
